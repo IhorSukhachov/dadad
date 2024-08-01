1fa3bf60b2b1786328c9ad3d18d65554b1965ce4
Ihor <i.sukhachov@gmail.com>
2024-08-01 10:35:02 +0300

    Initial Commit

---
+++ dadad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dadad
+//
+//  Created by Ihor Sukhachov on 01.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dadad/dadadApp.swift
@@ -0,0 +1,17 @@
+//
+//  dadadApp.swift
+//  dadad
+//
+//  Created by Ihor Sukhachov on 01.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct dadadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
